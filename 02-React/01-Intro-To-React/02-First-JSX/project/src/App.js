8b3734f7da6c906425769ddeaee7cf9a15269c01
AVWestover <thenamesrey@gmail.com>
2022-02-08 15:53:03 -0600

    First JSX

---
+++ 02-React/01-Intro-To-React/02-First-JSX/project/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // 
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <div className="list-div">
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+        </div>
+{/* comment for  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# MERN-Coding-Dojo-Assignments
+A repository for the assignments I did in the MERN stack at Coding Dojo
