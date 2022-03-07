3d6e19077c1e6143de030679acc2b0946d1b6844
AVWestover <thenamesrey@gmail.com>
2022-03-07 11:52:41 -0600

    "Jokes API" Assignment-update

--- 04-MongoDB/02-Mongoose/01-Jokes-API/server/server.js
+++ 04-MongoDB/02-Mongoose/01-Jokes-API/server/server.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = 8000;
     
-require("./config/mongoose.config");
-    
+
+require("./server/config/mongoose.config");
+
 app.use(express.json(), express.urlencoded({ extended: true }));
-    
-const AllMyJokeRoutes = require("./routes/joke.routes");
-AllMyJokeRoutes(app);
-    
-app.listen(8000, () => console.log("The server is all fired up on port 8000"));
+
+const JokeRoutes = require("./server/routes/jokes.routes");
+JokeRoutes(app);
+
+app.listen(port, () => console.log(`Listening on port: ${port}`) );
